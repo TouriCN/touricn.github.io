d8c391541cb45f56fda98eaf8a6dc0305d212727
TouriCN-Tourism <linyaovo20141128@qq.com>
2026-07-19 18:49:10 +0800

    更新 config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -11,19 +11,15 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/TouriCN/touricn.github.io' },
       {
-        icon: {
-          svg: `<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 1024 1024"><path fill="#FF8EB3" d="M512 138.667c-205.866 0-373.333 167.467-373.333 373.333s167.467 373.333 373.333 373.333 373.333-167.467 373.333-373.333-167.467-373.333-373.333-373.333zM384 469.333c-23.466 0-42.667 19.2-42.667 42.667s19.2 42.667 42.667 42.667 42.667-19.2 42.667-42.667-19.2-42.667-42.667-42.667zM640 469.333c-23.466 0-42.667 19.2-42.667 42.667s19.2 42.667 42.667 42.667 42.667-19.2 42.667-42.667-19.2-42.667-42.667-42.667zM512 768c-70.4 0-131.2-44.8-153.6-106.667h307.2C643.2 723.2 582.4 768 512 768z"/></svg>`
-        },
+        icon: 'simple-icons:bilibili',
         link: 'https://space.bilibili.com/3546574053443664',
         ariaLabel: 'Bilibili Space'
       },
       {
-        icon: {
-          svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><polyline points="22,6 12,13 2,6"/></svg>`
-        },
+        icon: 'mail',
         link: 'mailto:linyaovo20141128@qq.com',
         ariaLabel: 'Email'
-      }
-    ]
-  }
+      },
+    ],
+  },
 })
