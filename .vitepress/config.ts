13b40049aa1ec8b1c28827efc20b0f6149a09028
TouriCN-Tourism <linyaovo20141128@qq.com>
2026-07-19 12:16:47 +0800

    Add VitePress configuration for TouriCN site

---
+++ .vitepress/config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'TouriCN',
+  description: '神秘的站点。',
+  base: '/',           
+  outDir: '.vitepress/dist',
+
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+    ],
+    sidebar: [,
+      ],
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/TouriCN/touricn.github.io' },
+    ],
+  },
+})
