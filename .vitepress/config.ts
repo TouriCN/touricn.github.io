b29cdeea230186e36a178891b868e90288af5b30
TouriCN-Tourism <linyaovo20141128@qq.com>
2026-07-19 19:24:30 +0800

    更新 config.ts

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -9,9 +9,10 @@ export default defineConfig({
     nav: [{ text: '主页', link: '/' }],
     sidebar: [{ text: '主页', link: '/' }],
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/TouriCN/touricn.github.io'
-      { icon: {
-          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><rect x="4" y="6" width="16" height="12" rx="2" fill="none" stroke="#00AEEC" stroke-width="2"/><line x1="8" y1="14" x2="11" y2="11" stroke="#00AEEC" stroke-width="2" stroke-linecap="round"/><line x1="16" y1="14" x2="13" y2="11" stroke="#00AEEC" stroke-width="2" stroke-linecap="round"/><line x1="9" y1="3.5" x2="10" y2="6" stroke="#00AEEC" stroke-width="2.5" stroke-linecap="round"/><line x1="15" y1="3.5" x2="14" y2="6" stroke="#00AEEC" stroke-width="2.5" stroke-linecap="round"/></svg>'
+      { icon: 'github', link: 'https://github.com/TouriCN/touricn.github.io' },
+      {
+        icon: {
+          svg: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cmVjdCB4PSI0IiB5PSI2IiB3aWR0aD0iMTYiIGhlaWdodD0iMTIiIHJ4PSIyIiBmaWxsPSJub25lIiBzdHJva2U9IiMwMEFFRUMiIHN0cm9rZS13aWR0aD0iMiIvPjxsaW5lIHgxPSI4IiB5MT0iMTQiIHgyPSIxMSIgeTI9IjExIiBzdHJva2U9IiMwMEFFRUMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+PGxpbmUgeDE9IjE2IiB5MT0iMTQiIHgyPSIxMyIgeTI9IjExIiBzdHJva2U9IiMwMEFFRUMiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+PGxpbmUgeDE9IjkiIHkxPSIzLjUiIHgyPSIxMCIgeTI9IjYiIHN0cm9rZT0iIzAwQUVFQyIgc3Ryb2tlLXdpZHRoPSIyLjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIvPjxsaW5lIHgxPSIxNSIgeTE9IjMuNSIgeDI9IjE0IiB5Mj0iNiIgc3Ryb2tlPSIjMDBBRUVDIiBzdHJva2Utd2lkdGg9IjIuNSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+PC9zdmc+'
         },
         link: 'https://space.bilibili.com/3546574053443664',
         ariaLabel: 'Bilibili Space'
